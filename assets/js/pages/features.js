d96a52bb7de05e09c0135963b8abaa5135c8d228
Simon Asika <asika32764@gmail.com>
2025-02-18 22:45:37 +0800

    Fix JS

---
+++ assets/js/pages/features.js
@@ -0,0 +1,26 @@
+const items = document.querySelectorAll('.c-portfolio-item');
+for (const item of items) {
+  const imagesWrapper = item.querySelector('.c-portfolio-images');
+  const swiper = new Swiper(imagesWrapper, {
+    touchMove: true,
+    loop: true,
+    spaceBetween: 15
+  });
+  const prev = item.querySelector('[data-task=prev]');
+  const next = item.querySelector('[data-task=next]');
+  const pageCurrent = item.querySelector('.c-page__current');
+  prev.addEventListener('click', () => {
+    swiper.slidePrev();
+  });
+  next.addEventListener('click', () => {
+    swiper.slideNext();
+  });
+  swiper.on('slideChange', () => {
+    let i = (swiper.realIndex + 1).toString();
+    if (i.length === 1) {
+      i = '0' + i;
+    }
+    pageCurrent.textContent = i;
+  });
+}
+//# sourceMappingURL=features.js.map
